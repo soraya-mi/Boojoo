846175af5a9d90fd6997b4837f267e31c004e96b
soraya mehrabi <sorayami79@gmail.com>
2021-05-30 08:20:19 +0430

    mood tracker yearly report query changed.

--- lib/MoodTracker/moodTracker_database.dart
+++ lib/MoodTracker/moodTracker_database.dart
@@ -150,7 +150,6 @@ class MoodTrackerDataBaseHelper {
 
   //Argument should have format like "2021/" or "2022/"
   Future<List<Map<String, dynamic>>> getYearyLog(String LogDate) async {
-    debugPrint("infff");
     Database db = await this.database;
     List<Map<String, dynamic>> x = await db.rawQuery(
         'SELECT * from $moodTrackerTable WHERE $colDate LIKE "$LogDate%"');
