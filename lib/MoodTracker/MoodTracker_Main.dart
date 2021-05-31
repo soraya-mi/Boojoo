a112663f095d0810b606101827b675484a03977c
ghazalmohseni7 <82432729+ghazalmohseni7@users.noreply.github.com>
2021-05-31 09:25:55 +0430

    Update MoodTracker_Main.dart

--- lib/MoodTracker/MoodTracker_Main.dart
+++ lib/MoodTracker/MoodTracker_Main.dart
@@ -41,7 +41,7 @@ class _HomePage_MoodTrackerState extends State<HomePage_MoodTracker> {
   //     DateTime.now().month.toString() +
   //     '/' +
   //     DateTime.now().day.toString(); //change it later
-  String today="2021/5/11";
+  //String today="2021/5/11";
 
   String today = DateTime.now().year.toString() +
       '/' +
@@ -241,148 +241,7 @@ class _HomePage_MoodTrackerState extends State<HomePage_MoodTracker> {
       );
     },
   );
-=======
 
-  @override
-  Widget build(BuildContext context) => FutureBuilder(
-        future: getid(),
-        builder: (context, AsyncSnapshot<int> snapshot) {
-          String tmp = "";
-          int MoodIdTmp;
-          if (snapshot.hasData) {
-            moodLogId = snapshot.data;
-            debugPrint(moodLogId.toString());
-            debugPrint(log.toString());
-          } else {
-            debugPrint("loading");
-          }
-          return Scaffold(
-            appBar: AppBar(
-                brightness: Brightness.light,
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                leading: Container(
-                  margin: EdgeInsets.all(5),
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Colors.grey.withOpacity(0.5),
-                  ),
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(
-                      Icons.arrow_back,
-                      color: Colors.black,
-                    ),
-                  ),
-                )),
-            body: Center(
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'امروز حالت چه طور بود؟',
-                    ),
-                    SizedBox(height: 50),
-                    RatingBar.builder(
-                        initialRating: 5,
-                        itemCount: 5,
-                        itemBuilder: (context, index) {
-                          switch (index) {
-                            case 0:
-                              return Icon(
-                                //tmp="sentiment_very_dissatisfied",
-                                Icons.sentiment_very_dissatisfied,
-                                color: Colors.red,
-                              );
-
-                            case 1:
-                              return Icon(
-                                Icons.sentiment_dissatisfied,
-                                color: Colors.deepOrange,
-                              );
-                            case 2:
-                              return Icon(
-                                Icons.sentiment_neutral,
-                                color: Colors.amber,
-                              );
-                            case 3:
-                              return Icon(
-                                Icons.sentiment_satisfied,
-                                color: Colors.lightGreen,
-                              );
-                            case 4:
-                              return Icon(
-                                Icons.sentiment_very_satisfied,
-                                color: Colors.green,
-                              );
-                          }
-                        },
-                        onRatingUpdate: (rating) {
-                          //if(rating==0){print("hello");MoodIdTmp=0;}
-                          if (rating == 1) {
-                            print("hello1");
-                            MoodIdTmp = 1;
-                          }
-                          if (rating == 2) {
-                            print("hello2");
-                            MoodIdTmp = 2;
-                          }
-                          if (rating == 3) {
-                            print("hello3");
-                            MoodIdTmp = 3;
-                          }
-                          if (rating == 4) {
-                            print("hello4");
-                            MoodIdTmp = 4;
-                          }
-                          if (rating == 5) {
-                            print("hello5");
-                            MoodIdTmp = 5;
-                          }
-                        }),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Container(
-                      height: 50,
-                      width: MediaQuery.of(context).size.width * 0.5,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [Colors.amber, Colors.yellow],
-                          stops: [0, 1],
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                      ),
-                      child: InkWell(
-                        onTap: () {
-                          setState(() {
-                            SendToDB(MoodIdTmp, today);
-                          });
-                        },
-                        child: Center(
-                          child: Text(
-                            "ثبت",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.w600,
-                              fontSize: 15,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        },
-      );
 
 
   Future<int> getid() async {
